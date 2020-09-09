ddd2cb5b1d22e19390a0fad694207488313f5b19
Justin Piotroski <justin.piotroski@gmail.com>
2020-09-09 02:22:03 -0700

    Require the encoder

--- lib/taptag.rb
+++ lib/taptag.rb
@@ -4,6 +4,7 @@ require 'taptag/pn532/structs'
 require 'taptag/pn532/library_constants'
 require 'taptag/pn532/pn532'
 require 'taptag/nfc'
+require 'taptag/encoder'
 
 # Ruby gem for controlling the Waveshare PN532 NFC HAT
 module Taptag
