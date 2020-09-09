c3c6a80ac11b3fabf0df44eeb6987bd7e9b64476
Justin Piotroski <justin.piotroski@gmail.com>
2020-09-09 03:08:06 -0700

    Added logic for translating blocks back and forth, and a magic call method

--- lib/taptag/encoder.rb
+++ lib/taptag/encoder.rb
@@ -2,6 +2,16 @@ module Taptag
   # Handles translating messages to byte frames for writing to devices
   class Encoder
     class << self
+      def call(input, fmt = :mifare, blk_filter = writable_mifare_blocks)
+        if input.is_a?(String)
+          zip_blocks(slice_string(encode_string(input), fmt))
+        elsif identify_2d_array(input)
+          reduce_blocks(input, blk_filter)
+        elsif input.is_a?(Array)
+          slice_string(input, fmt)
+        end
+      end
+
       # Takes +str+ and converts the chars to ordinals
       def encode_string(str)
         str.chars.map(&:ord)
@@ -9,10 +19,12 @@ module Taptag
 
       # Takes +bytes+ and converts it back into a string
       def decode_string(bytes)
-        bytes.map(&:chr).join
+        bytes.map(&:chr)
+             .reject(&:zero?)
+             .join
       end
 
-      # Takes the +str+ and splits it into +blks+ frames
+      # Takes the +str+ and splits it into +blks+ frames with some shorthand
       def slice_string(str, blks)
         bloks = case blks
                 when :mifare
@@ -29,13 +41,33 @@ module Taptag
         frames
       end
 
+      # Takes in +blks+ and a +filter+ array to select blocks
+      def reduce_blocks(blks, filter = writable_mifare_blocks)
+        if identify_2d_array(blks)
+          hsh = blks.to_h
+          filter.map { |x| hsh[x] }.compact.flatten
+        else
+          filter.map { |x| blks[x] }.compact.flatten
+        end
+      end
+
+      def zip_blocks(blks, filter = writable_mifare_blocks)
+        filter.map
+              .with_index { |i, x| [i, blks[x]] }
+              .reject { |_a, b| b.nil? }
+      end
+
       # Returns just the write safe blocks for mifare
-      def writable_mifare_blocks(blocks = nil)
+      def writable_mifare_blocks
         @writable_mifare_blocks ||= (1..63).to_a - (0..15).map { |x| (4 * x) + 3 }
+      end
 
-        return @writable_mifare_blocks unless blocks
+      private
 
-        blocks.map.with_index { |i, x| [@writable_mifare_blocks[x], i] }
+      def identify_2d_array(arry)
+        arry.all? { |x| x.is_a?(Array) } &&
+          arry.all? { |x| x.length == 2 } &&
+          arry.all? { |x| x[0].is_a?(Integer) }
       end
     end
   end
