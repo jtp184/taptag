f37192d17cf4a18ed6254e4c407c553714294ab4
Justin Piotroski <justin.piotroski@gmail.com>
2020-09-09 16:40:47 -0700

    Refactor Encrypter into a module and allow more fine grained control over key / iv

--- lib/taptag/encrypter.rb
+++ lib/taptag/encrypter.rb
@@ -2,35 +2,31 @@ require 'openssl'
 
 module Taptag
   # Handles encrypting and decrypting data using OpenSSL algorithms
-  class Encrypter
-    # String used to instantiate the Cipher instance
-    attr_accessor :algorithm
+  module Encrypter
+    class << self
+      # Takes in a string +str+ and optional +algorithm+ and returns a
+      # hash with the key, vector, and encrypted data
+      def encrypt(str, args = {})
+        ec = OpenSSL::Cipher.new(args.fetch(:algorithm, 'AES-128-CBC'))
+        ec.encrypt
 
-    # Uses the +algo+ provided, or 'AES-128-CBC' by default
-    def initialize(algo = 'AES-128-CBC')
-      @algorithm = algo
-    end
+        key = args.key?(:key) ? ec.key = args[:key] : ec.random_key
+        vector = args.key?(:vector) ? ec.key = args[:vector] : ec.random_iv
 
-    # Takes in a string +str+ and returns a hash with the key, vector, and encrypted data
-    def encrypt(str)
-      ec = OpenSSL::Cipher.new(algorithm)
-      ec.encrypt
-      key = ec.random_key
-      vector = ec.random_iv
+        data = ec.update(str) + ec.final
 
-      data = ec.update(str) + ec.final
+        { key: key, vector: vector, data: data }
+      end
 
-      { key: key, vector: vector, data: data }
-    end
+      # Takes in +args+ for the key, vector, algorithm and data, and returns the decrypted string
+      def decrypt(args = {})
+        dc = OpenSSL::Cipher.new(args.fetch(:algorithm, 'AES-128-CBC'))
+        dc.decrypt
+        dc.key = args.fetch(:key)
+        dc.iv = args.fetch(:vector)
 
-    # Takes in +args+ for the key, vector, and data, and returns the decrypted string
-    def decrypt(args = {})
-      dc = OpenSSL::Cipher.new(algorithm)
-      dc.decrypt
-      dc.key = args.fetch(:key)
-      dc.iv = args.fetch(:vector)
-
-      dc.update(args.fetch(:data)) + dc.final
+        dc.update(args.fetch(:data)) + dc.final
+      end
     end
   end
 end
