9d824a511e079c4964f50f7ca9b320cec2fd01de
Justin Piotroski <justin.piotroski@gmail.com>
2020-09-08 22:07:18 -0700

    Added the card_uid class, and documentation

--- lib/taptag/nfc.rb
+++ lib/taptag/nfc.rb
@@ -1,14 +1,34 @@
 module Taptag
+  # The higher level interface to the hardware, implements reading and writing functions using ruby
+  # conventions.
   module NFC
     class << self
+      # Returns the firmware version reported by the device as a byte array
       def firmware_version
         buffer = PN532::DataBuffer.new
         PN532.get_firmware_version(pn_struct, buffer)
         buffer[1..3]
       end
 
+      # Invokes PN532#read_passive_target to get the uid length and return a byte array
+      def card_uid
+        buffer = PN532::DataBuffer.new
+
+        resp = PN532.read_passive_target(
+          pn_struct,
+          buffer,
+          PN532::MIFARE_ISO14443A,
+          1000
+        )
+
+        return if resp == PN532::STATUS_ERROR
+
+        buffer[0...resp]
+      end
+
       private
 
+      # Initializes and memoizes a PN532Struct for device control
       def pn_struct
         return @pn_struct if @pn_struct
 
