343f2cd8054ddbefb02ae52921066c7605193bde
Justin Piotroski <justin.piotroski@gmail.com>
2020-09-09 02:38:42 -0700

    Adds the slice_string method to break into blocks, and an optional argument to writable_mifare_blocks to create a 2D array

--- lib/taptag/encoder.rb
+++ lib/taptag/encoder.rb
@@ -7,9 +7,30 @@ module Taptag
         str.chars.map(&:ord)
       end
 
+      # Takes the +str+ and splits it into +blks+ frames
+      def slice_string(str, blks)
+        bloks = case blks
+                when :mifare
+                  16
+                when :ntag
+                  4
+                when ->(b) { b.is_a?(Integer) }
+                  blks
+                end
+
+        frames = str.each_slice(bloks).to_a
+        frames.last << 0 until frames.last.length == bloks
+
+        frames
+      end
+
       # Returns just the write safe blocks for mifare
-      def writable_mifare_blocks
+      def writable_mifare_blocks(blocks = nil)
         @writable_mifare_blocks ||= (1..63).to_a - (0..15).map { |x| (4 * x) + 3 }
+
+        return @writable_mifare_blocks unless blocks
+
+        blocks.map.with_index { |i, x| [@writable_mifare_blocks[x], i] }
       end
     end
   end
