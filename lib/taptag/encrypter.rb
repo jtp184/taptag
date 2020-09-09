48052ee96bd6d460320b5a6edf20a3f72cbd27ef
Justin Piotroski <justin.piotroski@gmail.com>
2020-09-09 15:51:14 -0700

    Rework encrypter to be a frontend for OpenSSL encryption

--- lib/taptag/encrypter.rb
+++ lib/taptag/encrypter.rb
@@ -1,19 +1,31 @@
+require 'openssl'
+
 module Taptag
   class Encrypter
     attr_accessor :algorithm
 
-    def initialize(algo)
-      @algorithm = if algo.is_a?(Symbol)
-                     method(algo)
-                   elsif algo.respond_to?(:call)
-                     algo
-                   else
-                     raise ArgumentError, "Can't pass string to #{algo.class}"
-                   end
+    def initialize(algo = 'AES-128-CBC')
+      @algorithm = algo
     end
 
-    def call(str)
-      algorithm.call(str)
+    def encrypt(str)
+      ec = OpenSSL::Cipher.new(algorithm)
+      ec.encrypt
+      key = ec.random_key
+      vector = ec.random_iv
+
+      data = ec.update(str) + ec.final
+
+      { key: key, vector: vector, data: data }
+    end
+
+    def decrypt(args = {})
+      dc = OpenSSL::Cipher.new(algorithm)
+      dc.decrypt
+      dc.key = args.fetch(:key)
+      dc.iv = args.fetch(:vector)
+
+      dc.update(args.fetch(:data)) + dc.final
     end
   end
 end
