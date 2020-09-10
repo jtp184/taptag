a50e93532fb5ae8161b977391ad9c72a781884c7
Justin Piotroski <justin.piotroski@gmail.com>
2020-09-10 03:32:57 -0700

    Add simple example for reading card uid

---
+++ bin/example/read_card_uid.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'taptag'
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org/'
+
+  gem 'colorize'
+  gem 'tty-spinner'
+end
+
+spinner = TTY::Spinner.new(
+  '[:spinner] Waiting for Tag...',
+  success_mark: '✅',
+  error_mark: '❗️',
+  frames: '🕒🕓🕔🕕🕖🕗🕘🕙🕚🕛🕐🕑',
+  interval: 12
+)
+
+spinner.auto_spin
+
+loop do
+  cuid = Taptag::NFC.card_uid
+  next unless cuid
+
+  spinner.stop('Found!'.green)
+
+  cuid.map! do |byte|
+    s = byte.to_s(16)
+    s.chars.one? ? "0x0#{s.upcase}" : "0x#{s.upcase}"
+  end
+
+  puts "[🏷️] #{cuid.inspect}"
+  exit
+end
