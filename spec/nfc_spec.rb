590a707c050ce89b540138813791367c11da715d
Justin Piotroski <justin.piotroski@gmail.com>
2020-09-12 18:11:30 -0700

    Basic beginning of test suite with tagged tests

--- spec/nfc_spec.rb
+++ spec/nfc_spec.rb
@@ -1,5 +1,9 @@
-RSpec.describe Taptag::NFC, :hardware do
+RSpec.describe 'Taptag::NFC', :hardware do
+  before do
+    require 'taptag/nfc'
+  end
+
   it 'can create a device struct' do
-    expect(subject.send(:pn_struct)).to be_a(Taptag::PN532::PN532Struct)
+    expect(Taptag::NFC.send(:pn_struct)).to be_a(Taptag::PN532::PN532Struct)
   end
 end
--- spec/pn532_spec.rb
+++ spec/pn532_spec.rb
@@ -1,5 +1,8 @@
-RSpec.describe Taptag::PN532 do
-  it 'extends FFI' do
+RSpec.describe 'Taptag::PN532', :hardware do
+  before do
+    require 'taptag/pn532/library_constants'
+    require 'taptag/pn532/structs'
+    require 'taptag/pn532/pn532'
   end
 
   %i[
@@ -11,7 +14,48 @@ RSpec.describe Taptag::PN532 do
     mifare_write_block
   ].map do |sig|
     it "implements #{sig}" do
-      raise NoMethodError unless subject.respond_to?(sig)
+      raise NoMethodError unless Taptag::PN532.respond_to?(sig)
+    end
+  end
+
+  it 'provides access to its library constants' do
+    expect(Taptag::PN532.lib_constants).to be_a(Hash)
+  end
+
+  describe 'structs' do
+    it 'can create device structs' do
+      d_struct = PN532::PN532Struct.new
+      expect(d_struct).not_to be_nil
+    end
+
+    describe 'DataBuffer' do
+      it 'can create blank databuffers' do
+        d = PN532::DataBuffer.new
+        expect(d).not_to be_nil
+        expect(d.to_a).to be_a(Array)
+      end
+
+      it 'can set databuffers to known values' do
+        rand_data = Array.new(255) { rand(255) }
+        d = PN532::DataBuffer.new.set(rand_data)
+
+        random_checks = (0..255).to_a.sample(12)
+
+        random_checks.each do |rc|
+          expect(d[rc]).to eq(rand_data[rc])
+        end
+      end
+
+      it 'can blank databuffers' do
+        d = PN532::DataBuffer.new.set(Array.new(255) { rand(255) })
+        d.reset
+        expect(d.to_a.all?(0)).to be(true)
+      end
+
+      it 'can return just the nonzero length of a buffer' do
+        d = PN532::DataBuffer.new.set(Array.new(128, 255))
+        expect(d.nonzero_length).to eq(128)
+      end
     end
   end
 end
