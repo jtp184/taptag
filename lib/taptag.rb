a0f75dcf7d6427337b566d326f19abe87005aa8d
Justin Piotroski <justin.piotroski@gmail.com>
2020-09-12 17:16:27 -0700

    Made changes to allow for nfc to be optional

--- lib/taptag.rb
+++ lib/taptag.rb
@@ -1,9 +1,5 @@
 require 'ffi'
 require 'taptag/version'
-require 'taptag/pn532/structs'
-require 'taptag/pn532/library_constants'
-require 'taptag/pn532/pn532'
-require 'taptag/nfc'
 require 'taptag/encoder'
 require 'taptag/encrypter'
 
