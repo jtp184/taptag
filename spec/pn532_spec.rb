37de0bbdf4628c683cae347aecb2d54c9c0c4ac5
Justin Piotroski <justin.piotroski@gmail.com>
2020-09-13 15:32:13 -0700

    Hotfix a missing module prefix

--- spec/pn532_spec.rb
+++ spec/pn532_spec.rb
@@ -24,20 +24,20 @@ RSpec.describe 'Taptag::PN532', :hardware do
 
   describe 'structs' do
     it 'can create device structs' do
-      d_struct = PN532::PN532Struct.new
+      d_struct = Taptag::PN532::PN532Struct.new
       expect(d_struct).not_to be_nil
     end
 
     describe 'DataBuffer' do
       it 'can create blank databuffers' do
-        d = PN532::DataBuffer.new
+        d = Taptag::PN532::DataBuffer.new
         expect(d).not_to be_nil
         expect(d.to_a).to be_a(Array)
       end
 
       it 'can set databuffers to known values' do
         rand_data = Array.new(255) { rand(255) }
-        d = PN532::DataBuffer.new.set(rand_data)
+        d = Taptag::PN532::DataBuffer.new.set(rand_data)
 
         random_checks = (0..255).to_a.sample(12)
 
@@ -47,13 +47,13 @@ RSpec.describe 'Taptag::PN532', :hardware do
       end
 
       it 'can blank databuffers' do
-        d = PN532::DataBuffer.new.set(Array.new(255) { rand(255) })
+        d = Taptag::PN532::DataBuffer.new.set(Array.new(255) { rand(255) })
         d.reset
         expect(d.to_a.all?(0)).to be(true)
       end
 
       it 'can return just the nonzero length of a buffer' do
-        d = PN532::DataBuffer.new.set(Array.new(128, 255))
+        d = Taptag::PN532::DataBuffer.new.set(Array.new(128, 255))
         expect(d.nonzero_length).to eq(128)
       end
     end
