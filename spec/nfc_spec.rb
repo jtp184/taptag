89cd7eaf4728191aeb1991b637aaec581238c6da
Justin Piotroski <justin.piotroski@gmail.com>
2020-09-13 15:46:31 -0700

    Added some basic NFC tests, including ones for when a mifare card is present

--- spec/nfc_spec.rb
+++ spec/nfc_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe 'Taptag::NFC', :hardware do
   it 'can create a device struct' do
     expect(Taptag::NFC.send(:pn_struct)).to be_a(Taptag::PN532::PN532Struct)
   end
+
+  it 'can get the firmware version' do
+    expect(Taptag::NFC.firmware_version).to eq([1, 6, 7])
+  end
 end
